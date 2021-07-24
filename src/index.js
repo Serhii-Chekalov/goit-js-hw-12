3d16aa3ff59854550260fb3a2d77b2c382f2a05f
Serhii Chekalov <78024336+Serhii-Chekalov@users.noreply.github.com>
2021-07-24 21:42:30 +0300

    add_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+import {getRefs} from './js/refs';
+
+import Notiflix from "notiflix";
+
+const debounce = require('lodash.debounce');
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+    return {
+    SearchCountry: document.querySelector('#search-box'),
+    CountryList: document.querySelector('.country-list'),
+    СountryInfo: document.querySelector('.country-info'),
+  }  }
